e255bebe7639b96d250b5c4faa1a9645f9b05fbf
Chaitanya <chaitanyagalla1813@gmail.com>
2025-08-02 12:59:24 +0530

    feat: added profile page to all users

--- src/hooks/useNetworkStatus.ts
+++ src/hooks/useNetworkStatus.ts
@@ -1,14 +1,30 @@
 import { useEffect, useState } from "react";
 
 const useNetworkStatus = () => {
-  const [isOnline, setIsOnline] = useState<boolean>(navigator.onLine);
+  const [isOnline, setIsOnline] = useState<boolean>(true); // Start with optimistic value
+
+  const checkInternetConnection = async () => {
+    try {
+      await fetch("https://www.google.com/generate_204", {
+        method: "GET",
+        mode: "no-cors",
+      });
+      setIsOnline(true);
+    } catch (error) {
+        console.log(error)
+      setIsOnline(false);
+    }
+  };
 
   useEffect(() => {
-    const handleOnline = () => setIsOnline(true);
+    const handleOnline = () => checkInternetConnection();
     const handleOffline = () => setIsOnline(false);
 
     window.addEventListener("online", handleOnline);
     window.addEventListener("offline", handleOffline);
+
+    // Initial check
+    checkInternetConnection();
 
     return () => {
       window.removeEventListener("online", handleOnline);
